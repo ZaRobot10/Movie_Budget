ec0c0d9dd63fa6fd73e70e95dfaeb81f861d9589
Ekankaar Khera <112088205+ZaRobot10@users.noreply.github.com>
2025-04-10 18:18:49 +0530

    Update next.config.ts

--- my_frontend/next.config.ts
+++ my_frontend/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
